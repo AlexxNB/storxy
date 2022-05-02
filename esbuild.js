d5209f7766367fd3791ed89e0b7ce43fe0397195
AlexxNB <iam@alexxnb.ru>
2022-05-02 12:58:44 +0300

    formatting build script

--- esbuild.js
+++ esbuild.js
@@ -5,34 +5,34 @@ const DEV = process.argv.includes('--dev');
 
 // common to all builds
 const common = {
-    entryPoints: ['./src/index.ts'],
-    minify: !DEV,
-    sourcemap: DEV && 'inline',
-    bundle: true
-}
+  entryPoints: ['./src/index.ts'],
+  minify: !DEV,
+  sourcemap: DEV && 'inline',
+  bundle: true
+};
 
 // module specific
 const builds = {
-    // Node module
-    cjs: {
-        outfile: pkg.main
-    },
-    // ES6 module
-    esm: {
-        outfile: pkg.module,
-    },
-    // Browser bundle
-    iife: {
-        globalName: 'Storxy',
-        outfile: DEV ? './test/visual/storxy.test.js' : pkg.cdn,
-    }
+  // Node module
+  cjs: {
+    outfile: pkg.main
+  },
+  // ES6 module
+  esm: {
+    outfile: pkg.module,
+  },
+  // Browser bundle
+  iife: {
+    globalName: 'Storxy',
+    outfile: DEV ? './test/visual/storxy.test.js' : pkg.cdn,
+  }
 };
 
 Object.keys(builds).forEach(key => {
-    const options = {
-        ...common,
-        ...builds[key],
-        format: key
-    }
-    build(options);
+  const options = {
+    ...common,
+    ...builds[key],
+    format: key
+  };
+  build(options);
 });
